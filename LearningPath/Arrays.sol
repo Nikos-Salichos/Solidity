64ec808081c30551c02b2ca2921bc03d4736f2c3
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2021-11-03 20:59:57 +0200

    Create Arrays.sol

---
+++ LearningPath/Arrays.sol
@@ -0,0 +1,26 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract learnArrays{
+    
+    uint[]  myArray;
+    
+    //Push adds one or more elements to the end and returns the new length of the myArray1
+    function pushMethod(uint number) public {
+        myArray.push(number);
+    }
+    
+    //Pop removes the last element from an array and returns the value to the caller.
+    function popMethod() public {
+        myArray.pop();
+    }
+    
+    //length is a string property that is used to determine the length of a string
+    function lengthMethod() public view returns (uint){
+       return myArray.length;
+    }
+    
+    //delete keyword do not change the length of the array, only the delete the value, index from 0 
+    function removeMethod(uint i)public{
+        delete myArray[i]; 
+    }
+}
