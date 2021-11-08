42b7c61ee6a6eeec3134ea57908996f9aa36cfe2
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2021-11-08 21:30:48 +0200

    Create CryptoCoin.sol

---
+++ CryptoCoin.sol
@@ -0,0 +1,35 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+//Allow only coin's owner to create to create new coins.
+
+contract CryptoCoin{
+
+    address public minter;
+    mapping(address => uint) public balances;
+    
+    event Sent(address from, address to, uint amount);
+    
+    //constructor run only onces
+    constructor(){
+        minter = msg.sender;
+    }
+    
+    //make new coins and send the to an address
+    //only the owner can send these coins
+    function mint(address receiver , uint amount) public{
+        require(msg.sender == minter);
+        balances[receiver] += amount;
+    }
+    
+    error insufficientBalance(uint requested, uint available);
+    
+    function send(address receiver, uint amount)public {
+        if(amount > balances[msg.sender])
+        revert insufficientBalance({
+            requested: amount,
+            available: balances[msg.sender]
+        });
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+    }
+}
