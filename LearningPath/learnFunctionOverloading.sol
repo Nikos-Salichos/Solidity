1cac6168789dfe9a9e41c74a7e9655605281c052
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2021-11-19 20:32:11 +0200

    Create learnFunctionOverloading.sol

---
+++ LearningPath/learnFunctionOverloading.sol
@@ -0,0 +1,26 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+//Multiple definitions for the same definition.
+//Each function definition should have different type/number of arguments
+//Cannot use overload for function that change only by return type.
+
+contract functionOverloading{
+    
+    function x(bool lightSwitch) public{
+        
+    }
+    
+    function x(uint wallet)public{
+        
+    }
+    
+    //Create 2 functions that
+    function exercise(uint a, uint b) public view returns(uint){
+        return a+b;
+    }
+    
+    function exercise(uint a, uint b, uint c) public view returns(uint){
+        return a+b+c;
+    }
+    
+}
