6d15f782ce2f66f8ed2b7ee78336730a0e1b0ca6
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2021-11-22 19:59:42 +0200

    Create Calculator.sol

---
+++ Calculator.sol
@@ -0,0 +1,25 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract learnFunctions  {
+   
+   function AddValues(uint a, uint b) public view returns (uint) {
+       uint result = a + b;
+       return result;
+    }   
+   
+   function SubtractValues(uint a, uint b) public view returns (uint) {
+       uint result = a - b;
+       return result;
+    }  
+  
+    function MultiplyValues(uint a, uint b) public view returns (uint) {
+        uint result = a * b;
+        return result;
+    }
+    
+    function DivideValues(uint a, uint b) public view returns (uint) {
+        uint result = a / b; //do not return fraction, only part before comma
+        return result;
+    }
+    
+}
