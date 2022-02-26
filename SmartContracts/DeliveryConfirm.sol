921b153d7a5567fc86739def5edf28d6df5262f6
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-02-26 12:32:11 +0200

    Update and rename NftStake.sol to DeliveryConfirm.sol

---
+++ SmartContracts/DeliveryConfirm.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+contract DeliveryConfirm{
+
+    enum State { 
+        AWAITING_PAYMENT, 
+        AWAITING_DELIVERY, 
+        COMPLETE 
+    }
+
+    
+    State public currentState;
+    
+    address public buyerAddress;
+    address payable public sellerAddress;
+    
+    modifier onlyBuyer() {
+        require(msg.sender == buyerAddress, "Only buyer can call this method");
+        _;
+    }
+    
+    constructor(address _buyer, address payable _seller)  {
+        buyerAddress = _buyer;
+        sellerAddress = _seller;
+    }
+    
+    function deposit() onlyBuyer external payable {
+        require(currentState == State.AWAITING_PAYMENT, "Already paid, you cannot pay twice");
+        currentState = State.AWAITING_DELIVERY;
+    }
+    
+    function confirmDelivery() onlyBuyer external {
+         require(currentState == State.AWAITING_DELIVERY, "Cannot confirm delivery from buyer");
+        sellerAddress.transfer(address(this).balance);
+        currentState = State.COMPLETE;
+    }
+}
