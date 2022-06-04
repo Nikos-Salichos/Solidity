f27114834ff1ccce9d7f37c17d45394f3096d5af
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-06-04 08:18:12 +0300

    Update BookRegistration.sol

--- SmartContracts/BookRegistration.sol
+++ SmartContracts/BookRegistration.sol
@@ -14,4 +14,11 @@ contract BookRegistration {
         
     event AddBook(address recipient, uint bookId);
     event SetFinished(uint bookId, bool finished);
+    
+    function addBook(string memory name, uint year, string memory author, bool finished) external{
+        uint bookId = bookList.length;
+        bookList.push(Book(bookId,name,year,author,finished));
+        bookToOwner[bookId] = msg.sender;
+        emit AddBook(msg.sender, bookId);
+    }
 }
