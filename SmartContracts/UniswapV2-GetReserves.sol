904f0467603aeee5ee99bc33c9fa921e3b6645e8
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-07-16 09:19:41 +0300

    Update and rename Get-Reserves-in-Solidity-(Example-Uniswap-V2).sol to UniswapV2-GetReserves.sol

--- SmartContracts/UniswapV2-GetReserves.sol
+++ SmartContracts/UniswapV2-GetReserves.sol
@@ -15,12 +15,12 @@ interface UniswapV2Pair{
 
 contract UniswapExample {
     address private factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f ; 
-    address private dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address private weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+   // address private dai = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
+  //  address private weth = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
     
-    function getTokenReserves() external view returns (uint , uint){
+    function getTokenReserves(address token0, address token1) external view returns (uint , uint){
         
-        address pair = UniswapV2Factory(factory).getPair(dai, weth);
+        address pair = UniswapV2Factory(factory).getPair(token0, token1);
         
         (uint reserve0, uint reserve1,) = UniswapV2Pair(pair).getReserves();
         
