28ad47f83743f1136a3f87da71132f0062096ed6
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-08-14 19:43:34 +0300

    Update NewNFT.sol

--- SmartContracts/NewNFT.sol
+++ SmartContracts/NewNFT.sol
@@ -2,3 +2,4 @@
 pragma solidity 0.8.0;
 
 import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
