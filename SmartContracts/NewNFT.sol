559182687d8cd36fcac63390c2810d480baf4562
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-08-15 07:41:45 +0300

    Update NewNFT.sol

--- SmartContracts/NewNFT.sol
+++ SmartContracts/NewNFT.sol
@@ -13,7 +13,7 @@ contract newNFT is NFTokenMetadata, Ownable {
   
    function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
      super._mint(_to, _tokenId);
-
+     super._setTokenUri(_tokenId, _uri);
   }
   
 }
