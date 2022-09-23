6fa7515f453afe8c2fa28c7e1930347b7e5fd659
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-09-23 20:10:49 +0300

    Update CRUD.sol

--- SmartContracts/CRUD.sol
+++ SmartContracts/CRUD.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.8.17;
 contract Ownable  {
   address public owner;
   event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 }
 
 
