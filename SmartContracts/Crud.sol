bfb475e7edb120b816f255ccd9b17621ad3d8989
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-11-06 10:27:10 +0200

    Update Crud.sol

--- SmartContracts/Crud.sol
+++ SmartContracts/Crud.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.17;
-
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.7;
 
 contract Ownable  {
   address public owner;
@@ -30,57 +29,7 @@ contract Ownable  {
 }
 
 
-contract Authorizable is Ownable {
-
-    mapping(address => bool) public authorized;
-
-    event AuthorizableAddressAdded(address addr);
-    event AuthorizableAddressRemoved(address addr);
-
-    modifier onlyAuthorized() {
-        require(authorized[msg.sender] || owner == msg.sender);
-        _;
-    }
-    
-    function addAuthorizedAddress(address addr) onlyOwner public returns(bool success) {
-        if (!authorized[addr]) {
-            authorized[addr] = true;
-            emit AuthorizableAddressAdded(addr);
-            success = true; 
-        }
-    }
-    
-    
-    function addAuthorizedAddresses(address[] memory addrs) onlyOwner public returns(bool success) {
-        for (uint256 i = 0; i < addrs.length; i++) {
-            if (addAuthorizedAddress(addrs[i])) {
-                success = true;
-            }
-        }
-    }
-    
-     function removeAddressFromAuthorized(address addr) onlyOwner public returns(bool success) {
-        if (authorized[addr]) {
-            authorized[addr] = false;
-            emit AuthorizableAddressRemoved(addr);
-            success = true;
-        }
-    }
-    
-     function removeAddressesFromWhitelist(address[] memory addrs) onlyOwner public returns(bool success) {
-         for (uint256 i = 0; i < addrs.length; i++) {
-             if (removeAddressFromAuthorized(addrs[i])) {
-              success = true;
-             }
-         }
-     }
-    
-    
-}
-
-contract CRUD {
-
-    address public owner;
+contract Crud is Ownable{
     uint256 public activeProductCounter = 0;
     uint256 public inactiveProductCounter = 0;
     uint256 private productCounter = 0;
@@ -90,7 +39,7 @@ contract CRUD {
     mapping(address => uint256) public productsOf;
 
     enum Deactivated { NO, YES }
-    
+
     struct ProductStruct {
         uint256 productId;
         string title;
@@ -100,10 +49,10 @@ contract CRUD {
         uint256 created;
         uint256 updated;
     }
-    
+
     ProductStruct[] activeProducts;
     ProductStruct[] inactiveProducts;
-    
+
     event Action (
         uint256 productId,
         string actionType,
@@ -111,5 +60,11 @@ contract CRUD {
         address indexed executor,
         uint256 created
     );
+
+    constructor() {
+        owner = msg.sender;
+    }
     
-}
+    
+    
+ }
