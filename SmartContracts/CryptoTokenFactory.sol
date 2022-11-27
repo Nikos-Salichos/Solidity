19d565431f983dcd0bbc292e02b11912ddc97202
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-11-27 19:39:15 +0200

    Update CryptoTokenFactory.sol

--- SmartContracts/CryptoTokenFactory.sol
+++ SmartContracts/CryptoTokenFactory.sol
@@ -1,4 +1,9 @@
 
 contract CryptoCoinFactory{ 
+
    CryptoCoin[] public CryptoCoins;
+   
+   function createCryptoCoin(uint256 initialSupply, string memory tokenName, string memory tokenSymbol, uint256 tokenCap) public {
+
+    }
 }
