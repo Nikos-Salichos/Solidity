7d7c470546cdaf3cc65fc739a913e9853df161bc
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-11-28 21:48:25 +0200

    Update Airdrop.sol

--- SmartContracts/Airdrop.sol
+++ SmartContracts/Airdrop.sol
@@ -4,5 +4,6 @@ pragma solidity >=0.8.0;
 interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
-    
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
 }
