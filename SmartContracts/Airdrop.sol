f3fc3c4f21537f2af580f986cc681633efe9f042
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-11-28 21:50:16 +0200

    Update Airdrop.sol

--- SmartContracts/Airdrop.sol
+++ SmartContracts/Airdrop.sol
@@ -20,6 +20,8 @@ contract Airdrop {
     
     receive() external payable{}
     
+    function airdropToken(address _tokenAddress, address[] memory _recipients, uint256 _amount) public {
 
+    }
 
 }
