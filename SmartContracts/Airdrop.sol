c7f7a851f59fef26e688e84b7ebc58a4b85ae1ca
Nikos-Salichos <70713775+Nikos-Salichos@users.noreply.github.com>
2022-11-28 21:49:47 +0200

    Update Airdrop.sol

--- SmartContracts/Airdrop.sol
+++ SmartContracts/Airdrop.sol
@@ -9,3 +9,10 @@ interface IERC20 {
     function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
+
+contract Airdrop {
+    
+    address owner;
+    
+
+}
